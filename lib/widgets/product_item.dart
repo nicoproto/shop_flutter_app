b3cb070f085b730adfe9e870e5ae9abda2dbd0b8
Nico Proto <nicolasproto@gmail.com>
2021-10-13 15:56:47 +0200

    Using 'Consumer' instead of 'Provider.of'

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -14,42 +14,44 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(10),
-      child: GridTile(
-        child: GestureDetector(
-          child: Image.network(
-            product.imageUrl, fit: BoxFit.cover,),
-          onTap: () {
-            Navigator.of(context).pushNamed(
-              ProductDetailScreen.routeName,
-              arguments: product.id,
-            );
-          },
-        ),
-        footer: GridTileBar(
-          backgroundColor: Colors.black87,
-          leading: IconButton(
-            onPressed: () {
-              product.toggleFavoriteStatus();
+    // final product = Provider.of<Product>(context);
+    return Consumer<Product>(
+      builder: (ctx, product, child) => ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: GridTile(
+          child: GestureDetector(
+            child: Image.network(
+              product.imageUrl, fit: BoxFit.cover,),
+            onTap: () {
+              Navigator.of(context).pushNamed(
+                ProductDetailScreen.routeName,
+                arguments: product.id,
+              );
             },
-            icon: Icon(
-              product.isFavorite ? Icons.favorite : Icons.favorite_border
+          ),
+          footer: GridTileBar(
+            backgroundColor: Colors.black87,
+            leading: IconButton(
+              onPressed: () {
+                product.toggleFavoriteStatus();
+              },
+              icon: Icon(
+                product.isFavorite ? Icons.favorite : Icons.favorite_border
+              ),
+              color: Theme.of(context).accentColor,
             ),
-            color: Theme.of(context).accentColor,
+            title: Text(
+              product.title,
+              textAlign: TextAlign.center,
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.shopping_cart),
+              color: Theme.of(context).accentColor,
+            ),
           ),
-          title: Text(
-            product.title,
-            textAlign: TextAlign.center,
-          ),
-          trailing: IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.shopping_cart),
-            color: Theme.of(context).accentColor,
-          ),
-        ),
 
+        ),
       ),
     );
   }
