f67bd5202635ed5847a74cb7a91689208768761b
Nico Proto <nicolasproto@gmail.com>
2021-10-13 13:47:41 +0200

    Exploring alternative provider syntaxes

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../providers/product.dart';
 import '../providers/products.dart';
 import '../widgets/product_item.dart';
 
@@ -14,8 +13,8 @@ class ProductsGrid extends StatelessWidget {
     return GridView.builder(
       padding: const EdgeInsets.all(10.0),
       itemCount: products.length,
-      itemBuilder: (ctx, i) => ChangeNotifierProvider(
-        create: (c) => products[i],
+      itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
+        value: products[i],
         child: ProductItem(
           // products[i].id,
           // products[i].title,
