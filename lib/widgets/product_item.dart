c2def785d5a81d29509dc4543bdda37ca06d16c5
Nico Proto <nicolasproto@gmail.com>
2021-10-15 21:26:38 +0200

    Using ScaffoldMessenger for Snackbars

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -49,8 +49,8 @@ class ProductItem extends StatelessWidget {
             trailing: IconButton(
               onPressed: () {
                 cart.addItem(product.id, product.title, product.price);
-                Scaffold.of(context).hideCurrentSnackBar();
-                Scaffold.of(context).showSnackBar(
+                ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                ScaffoldMessenger.of(context).showSnackBar(
                   SnackBar(
                     content: Text('Added item to cart!',),
                     duration: Duration(seconds: 4),
