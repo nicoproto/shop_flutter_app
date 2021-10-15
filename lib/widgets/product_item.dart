637b538c7aa85cfcc5e42a4075d7847ad8c67a56
Nico Proto <nicolasproto@gmail.com>
2021-10-15 21:25:40 +0200

    Snackbars & Doing 'Add to Cart' Actions

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -49,6 +49,19 @@ class ProductItem extends StatelessWidget {
             trailing: IconButton(
               onPressed: () {
                 cart.addItem(product.id, product.title, product.price);
+                Scaffold.of(context).hideCurrentSnackBar();
+                Scaffold.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text('Added item to cart!',),
+                    duration: Duration(seconds: 4),
+                    action: SnackBarAction(
+                      label: 'UNDO',
+                      onPressed: () {
+                        cart.removeSingleItem(product.id);
+                      },
+                    ),
+                  )
+                );
               },
               icon: Icon(Icons.shopping_cart),
               color: Theme.of(context).accentColor,
