070a5cf0e6f42733d3eaba37013095d371fc04e8
Nico Proto <nicolasproto@gmail.com>
2021-10-16 17:23:09 +0200

    Allowing users to delete products

--- lib/widgets/user_product_item.dart
+++ lib/widgets/user_product_item.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_flutter_app/providers/products.dart';
 
 import '../screens/edit_product_screen.dart';
 
@@ -31,7 +33,9 @@ class UserProductItem extends StatelessWidget {
               color: Theme.of(context).primaryColor,
             ),
             IconButton(
-              onPressed: () {},
+              onPressed: () {
+                Provider.of<Products>(context, listen: false).deleteProduct(id);
+              },
               icon: Icon(Icons.delete),
               color: Theme.of(context).errorColor,
             ),
