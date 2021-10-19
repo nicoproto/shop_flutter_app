51c83d6520729638baf815719f9febd0906cb360
Nico Proto <nicolasproto@gmail.com>
2021-10-19 16:48:00 +0200

    Automatically logging in

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shop_flutter_app/screens/products_overview_screen.dart';
 
 import './providers/auth.dart';
 import './providers/products.dart';
 import './providers/cart.dart';
 import './providers/orders.dart';
 
+import './screens/products_overview_screen.dart';
+import './screens/splash_screen.dart';
 import './screens/auth_screen.dart';
 import './screens/edit_product_screen.dart';
 import './screens/user_products_screen.dart';
@@ -44,7 +45,14 @@ class MyApp extends StatelessWidget {
             accentColor: Colors.deepOrange,
             fontFamily: 'Lato',
           ),
-          home: auth.isAuth ? ProductsOverviewScreen() : AuthScreen(),
+          home: auth.isAuth
+            ? ProductsOverviewScreen()
+            : FutureBuilder(
+              future: auth.tryAutoLogin(),
+              builder: (ctx, authResultSnapshot ) =>
+                authResultSnapshot.connectionState == ConnectionState.waiting
+                  ? SplashScreen()
+                  : AuthScreen(),),
           routes: {
             AuthScreen.routeName: (ctx) => AuthScreen(),
             ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
