6d338f8463130a8e0e0941767ba520082719bb53
Nico Proto <nicolasproto@gmail.com>
2021-10-19 11:23:44 +0200

    Adding user sign in

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -8,8 +8,9 @@ class Auth with ChangeNotifier {
   DateTime _expiryDate;
   String _userId;
 
-  Future<void> signup(String email, String password) async {
-    final url = Uri.parse('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCz8i6Lxb8CDO40MOl8noipvHpJ2x1gKRI');
+  Future<void> _authenticate(String email, String password, String urlSegment) async {
+    final url = Uri.parse(
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyCz8i6Lxb8CDO40MOl8noipvHpJ2x1gKRI');
 
     final response = await http.post(
       url,
@@ -19,7 +20,18 @@ class Auth with ChangeNotifier {
         'returnSecureToken': true,
       }),
     );
+  }
 
-    print(json.decode(response.body));
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, 'signUp');
+  }
+
+  Future<void> login(String email, String password) async {
+    return _authenticate(email, password, 'signInWithPassword');
+
+    // final responseData = json.decode(response.body);
+    // _token = responseData['idToken'];
+    // _userId = responseData['localId'];
+    // _expiryDate = DateTime.now() + responseData['expiresIn'];
   }
 }
