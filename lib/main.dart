cbe041c59a9915acf31de968c3935a885e6808b1
Nico Proto <nicolasproto@gmail.com>
2021-10-19 11:11:33 +0200

    Adding user signup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import './providers/auth.dart';
 import './providers/products.dart';
 import './providers/cart.dart';
 import './providers/orders.dart';
@@ -10,7 +11,6 @@ import './screens/edit_product_screen.dart';
 import './screens/user_products_screen.dart';
 import './screens/orders_screen.dart';
 import './screens/product_detail_screen.dart';
-import './screens/products_overview_screen.dart';
 import './screens/cart_screen.dart';
 
 void main() => runApp(MyApp());
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(providers: [
+      ChangeNotifierProvider(create: (_) => Auth()),
       ChangeNotifierProvider(create: (_) => Products()),
       ChangeNotifierProvider(create: (_) => Cart()),
       ChangeNotifierProvider(create: (_) => Orders()),
