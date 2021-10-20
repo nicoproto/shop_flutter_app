2ea193420fce7ace28438ef35da1b85465ab861e
Nico Proto <nicolasproto@gmail.com>
2021-10-20 12:48:47 +0200

    Animating Order Boxes

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -18,43 +18,52 @@ class _OrderItemState extends State<OrderItem> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.all(10),
-      child: Column(
-        children: <Widget>[
-          ListTile(
-            title: Text('\$${widget.order.amount}'),
-            subtitle: Text(DateFormat('dd/MM/yy hh:mm').format(widget.order.dateTime)),
-            trailing: IconButton(
-              onPressed: () {
-                setState(() {
-                  _expanded = !_expanded;
-                });
-              },
-              icon: Icon(_expanded ? Icons.expand_more : Icons.expand_less)
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 300),
+      height: _expanded
+      ? min(widget.order.products.length * 20.0 + 120, 200)
+      : 95,
+      child: Card(
+        margin: EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            ListTile(
+              title: Text('\$${widget.order.amount}'),
+              subtitle: Text(DateFormat('dd/MM/yy hh:mm').format(widget.order.dateTime)),
+              trailing: IconButton(
+                onPressed: () {
+                  setState(() {
+                    _expanded = !_expanded;
+                  });
+                },
+                icon: Icon(_expanded ? Icons.expand_more : Icons.expand_less)
+              ),
             ),
-          ),
-          if (_expanded) Container(
-            height: min(widget.order.products.length * 20.0 + 100, 100),
-            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-            child: ListView.builder(
-              itemCount: widget.order.products.length,
-              itemBuilder: (ctx, i) => Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    widget.order.products[i].title,
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    '${widget.order.products[i].quantity}x \$${widget.order.products[i].price}',
-                    style: TextStyle(fontSize: 18, color: Colors.grey),
-                  )
-                ],
-              )
-            ),
-          )
-        ],
+            AnimatedContainer(
+              duration: Duration(milliseconds: 300),
+              height: _expanded
+              ? min(widget.order.products.length * 20.0 + 18, 200)
+              : 0,
+              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+              child: ListView.builder(
+                itemCount: widget.order.products.length,
+                itemBuilder: (ctx, i) => Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      widget.order.products[i].title,
+                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      '${widget.order.products[i].quantity}x \$${widget.order.products[i].price}',
+                      style: TextStyle(fontSize: 18, color: Colors.grey),
+                    )
+                  ],
+                )
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
